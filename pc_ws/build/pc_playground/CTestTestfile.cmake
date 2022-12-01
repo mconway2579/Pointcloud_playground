c39064d800dc1e5aac1a6221240b729f5139ccd8
mconway2579 <maxlconway@gmail.com>
2022-12-01 16:55:08 -0700

    remove devel and build

--- pc_ws/build/pc_playground/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/max/pc_ws/src/pc_playground
-# Build directory: /home/max/pc_ws/build/pc_playground
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
